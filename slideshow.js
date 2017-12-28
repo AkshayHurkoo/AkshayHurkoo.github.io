8cb2baa650a6b63d2b8cda1b5e2e8af4da4de1d0
Akshay hurkoo <p15200588@my365.dmu.ac.uk>
2017-12-28 19:20:44 +0000

    test side menu draft

--- slideshow.js
+++ slideshow.js
@@ -1,23 +1,30 @@
-
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("myslides");
-    var dots = document.getElementsByClassName("myslides");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 8000); // Change image every 8 seconds
-     
+  var i;
+  var slides = document.getElementsByClassName("myslides");
+  var dots = document.getElementsByClassName("myslides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
+
 }
 
-
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle('active');
+  document.getElementById("sidebar").style.width = "200px";
+  document.getElementById("Container").style.marginLeft = "250px";
+  document.getElementById("comingSoon").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
